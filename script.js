324c4d7f5591773abfd814cf16559025c76161c0
get821 <s34354@banbung.ac.th>
2026-09-14 11:04:07 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+// เลื่อนหน้าเว็บแบบนุ่มนวล
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+
+  link.addEventListener("click", function(event) {
+
+    const target = document.querySelector(
+      this.getAttribute("href")
+    );
+
+    if (target) {
+
+      event.preventDefault();
+
+      target.scrollIntoView({
+        behavior: "smooth"
+      });
+
+    }
+
+  });
+
+});
